4873fd8080d9d6524efecba657548a1ad1541471
Danilo Vaz <131407565+danilovaz7@users.noreply.github.com>
2025-05-31 09:48:35 -0300

    Update loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -25,7 +25,7 @@ async function login(req, res) {
     const token = jwt.sign(
         { id: user.id, idTipo: user.tipo_usuario_id },
         process.env.SECRET_KEY,
-        { expiresIn: '1h' }
+        { expiresIn: '24h' }
     );
 
     return res.status(200).json({ token });
